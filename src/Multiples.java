119f1cd3d24b7583a347d2f75aa845ef7b94a89e
Cristiano Afonso da Silva <cristianoafonsodasilva@Cristianos-MacBook-Pro.local>
2023-09-11 12:48:47 -0400

    refactored to extract helper method

---
+++ src/Multiples.java
@@ -0,0 +1,13 @@
+public class Multiples {
+    public static void main(String[] args) {
+        int k = 0;
+        for (int i = 1; i < 1000; i++) {
+            boolean divisibleBy3 = i % 3 == 0;
+            boolean divisibleBy5 = i % 5 == 0;
+            if (divisibleBy5 || divisibleBy3) {
+                k++;
+            }
+        }
+        System.out.println(k);
+    }
+}
