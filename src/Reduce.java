e369a0aff70f8f182f5add8370ea4775e71ecd38
Cristiano Afonso da Silva <cristianoafonsodasilva@Cristianos-MacBook-Pro.local>
2023-09-11 12:49:30 -0400

    refactored to extract helper method

--- src/Reduce.java
+++ src/Reduce.java
@@ -1,2 +1,15 @@
-package PACKAGE_NAME;public class Reduce {
+public class Reduce {
+    public static void main(String[] args) {
+        int i = 100;
+
+        while (i > 0){
+            if (i % 2 == 0) {
+                i = i / 2;
+            }
+            else {
+                i = i - 1;
+            }
+            System.out.println(i);
+        }
+    }
 }
